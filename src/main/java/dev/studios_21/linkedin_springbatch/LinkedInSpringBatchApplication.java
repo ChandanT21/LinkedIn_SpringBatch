3ab0ad0e2301539cc1c97e9f20c61366c0e20764
ChandanT21 <vichandan@me.com>
2022-12-12 20:49:42 -0500

    Completed Lesson 4: ItemReader FlatFile and Jdbc

--- src/main/java/dev/studios_21/linkedin_springbatch/LinkedInSpringBatchApplication.java
+++ src/main/java/dev/studios_21/linkedin_springbatch/LinkedInSpringBatchApplication.java
@@ -12,6 +12,10 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.PagingQueryProvider;
+import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
@@ -23,6 +27,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 
+import javax.sql.DataSource;
 import java.io.File;
 import java.util.List;
 
@@ -30,7 +35,11 @@ import java.util.List;
 @EnableBatchProcessing
 public class LinkedInSpringBatchApplication {
 
+    @Autowired
+    public DataSource dataSource;
+    public static String ORDER_SQL = "SELECT * from SHIPPED_ORDER ORDER BY order_id";
     public static String[] header = new String[] {"order_id", "first_name","last_name","email","cost","item_id","item_name","ship_date"};
+
 
     @Autowired
     public JobBuilderFactory jobBuilderFactory;
@@ -39,21 +48,40 @@ public class LinkedInSpringBatchApplication {
     public StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Step chunkBasedStep() {
-        return this.stepBuilderFactory.get("chunkBasedStep")
-                .<Order,Order>chunk(3)
-                .reader(itemReader())
-                .writer(new ItemWriter<Order>() {
-                    @Override
-                    public void write(List<? extends Order> list) throws Exception {
-                        System.out.printf("Received list of size: %s \n", list.size());
-                        list.forEach(System.out::println);
-                    }
-                }).build();
+    public PagingQueryProvider queryProvider() throws Exception {
+        SqlPagingQueryProviderFactoryBean factoryBean = new SqlPagingQueryProviderFactoryBean();
+        factoryBean.setSelectClause("SELECT order_id, first_name, last_name, email, cost, item_id, item_name, ship_date");
+        factoryBean.setFromClause("FROM SHIPPED_ORDER");
+        factoryBean.setSortKey("order_id");
+        factoryBean.setDataSource(dataSource);
+        return factoryBean.getObject();
     }
 
     @Bean
-    public ItemReader<Order> itemReader() {
+    public ItemReader<Order> itemReader () throws Exception {
+        JdbcPagingItemReader<Order> itemReader = new JdbcPagingItemReader<Order>();
+        itemReader.setDataSource(dataSource);
+        itemReader.setName("jdbcPagingItemReader");
+        itemReader.setQueryProvider(queryProvider());
+        itemReader.setRowMapper(new OrderRowMapper());
+        itemReader.setPageSize(10); //sync with chunk size
+        return itemReader;
+    }
+
+    @Bean
+    public ItemReader<Order> itemReader_JdbcCursor_forSingleThread () {
+        JdbcCursorItemReader<Order> itemReader = new JdbcCursorItemReader<Order>();
+        itemReader.setDataSource(dataSource);
+        itemReader.setName("jdbcCursorItemReader");
+        itemReader.setSql(ORDER_SQL);
+        itemReader.setRowMapper(new OrderRowMapper());
+        return itemReader;
+    }
+
+
+
+    @Bean
+    public ItemReader<Order> itemReader_FlatFile() {
         FlatFileItemReader<Order> itemReader = new FlatFileItemReader<>();
         itemReader.setLinesToSkip(1); //skip header
         itemReader.setResource(new ClassPathResource("/data/shipped_orders.csv"));
@@ -72,6 +100,24 @@ public class LinkedInSpringBatchApplication {
         return this.jobBuilderFactory.get("job")
                 .start(chunkBasedStep())
                 .build();
+    }
+
+    @Bean
+    public Step chunkBasedStep() {
+        try {
+            return this.stepBuilderFactory.get("chunkBasedStep")
+                    .<Order,Order>chunk(10)
+                    .reader(itemReader())
+                    .writer(new ItemWriter<Order>() {
+                        @Override
+                        public void write(List<? extends Order> list) throws Exception {
+                            System.out.printf("Received list of size: %s \n", list.size());
+                            list.forEach(System.out::println);
+                        }
+                    }).build();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/dev/studios_21/linkedin_springbatch/OrderRowMapper.java
@@ -0,0 +1,23 @@
+package dev.studios_21.linkedin_springbatch;
+
+import entity.Order;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class OrderRowMapper implements org.springframework.jdbc.core.RowMapper<entity.Order> {
+    
+    @Override
+    public Order mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Order order = new Order();
+        order.setOrderId(rs.getLong("order_id"));
+        order.setCost(rs.getBigDecimal("cost"));
+        order.setEmail(rs.getString("email"));
+        order.setFirstName(rs.getString("first_name"));
+        order.setLastName(rs.getString("last_name"));
+        order.setItemId(rs.getString("item_id"));
+        order.setItemName(rs.getString("item_name"));
+        order.setShipDate(rs.getDate("ship_date"));
+        return order;
+    }
+}
